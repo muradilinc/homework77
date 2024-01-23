cd48884ca00337d2ef1df4e2f0c6742dad75293b
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-01-23 18:11:41 +0600

    add cors

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import notationRouter from "./routes/notation";
+import cors from 'cors';
 
 const app = express();
 const port = 8000;
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cors());
+
 app.use('/notations', notationRouter);
 
 app.listen(port, () => {
